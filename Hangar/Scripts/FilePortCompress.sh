0a8aefe375d71dfb091568edaa836852cf98611a
Ubuntu <azureuser@OriginHangar.2ktfglbbd5qupcqluqsrn4fpah.xx.internal.cloudapp.net>
2024-10-25 10:55:27 -0700

    Encryption Option Added

--- Hangar/Scripts/FilePortCompress.sh
+++ Hangar/Scripts/FilePortCompress.sh
@@ -39,6 +39,8 @@ RUNWAY_FOLDER=$(jq -r '.RunWay_Folder' "$DIRECTORIES_CONFIG_FILE")
 BACKUP_FILE_NAME=$(jq -r '.backup_file_name' "$DIRECTORIES_CONFIG_FILE")
 DIRECTORIES=($(jq -r '.directories[]' "$DIRECTORIES_CONFIG_FILE"))
 EXCLUDE=($(jq -r '.exclude[]' "$DIRECTORIES_CONFIG_FILE"))
+ENCRYPTION_ENABLED=$(jq -r '.encryption_enabled' "$DIRECTORIES_CONFIG_FILE")
+ENCRYPTION_KEY=$(jq -r '.encryption_key' "$DIRECTORIES_CONFIG_FILE")
 
 # Create RunWay and log folders if they don't exist
 mkdir -p "$RUNWAY_FOLDER"
@@ -50,11 +52,13 @@ LOG_FILE="$LOG_FOLDER/${LOG_FILE_NAME}_${SERVER_NAME}_$(date +'%Y%m%d_%H%M%S').l
 # Start logging
 echo "Compression started at $(date +'%Y-%m-%d %H:%M:%S') on $SERVER_NAME" | tee "$LOG_FILE"
 
-# Compress each directory with maximum compression using xz and exclude specified directories
+# Compress and encrypt each directory
 for DIR in "${DIRECTORIES[@]}"; do
     BASENAME=$(basename "$DIR")
-    COMPRESSED_FILE="$RUNWAY_FOLDER/${SERVER_NAME}_${BACKUP_FILE_NAME}.tar.xz"
-    echo "Compressing $DIR to $COMPRESSED_FILE" | tee -a "$LOG_FILE"
+    COMPRESSED_FILE="$RUNWAY_FOLDER/${SERVER_NAME}_${BACKUP_FILE_NAME}_${BASENAME}.tar.xz"
+    ENCRYPTED_FILE="${COMPRESSED_FILE}.gpg"
+    
+    echo "Compressing and encrypting $DIR to $ENCRYPTED_FILE" | tee -a "$LOG_FILE"
     
     # Build the exclude options for tar and handle them as relative paths
     EXCLUDE_OPTIONS=()
@@ -66,11 +70,11 @@ for DIR in "${DIRECTORIES[@]}"; do
     # Debugging output to check exclude options
     echo "Exclude options: ${EXCLUDE_OPTIONS[@]}" | tee -a "$LOG_FILE"
     
-    # Run the tar command to compress the directory with xz and exclude specified directories using relative paths
-    if tar -cJvf "$COMPRESSED_FILE" "${EXCLUDE_OPTIONS[@]}" -C "$(dirname "$DIR")" "$BASENAME" 2>> "$LOG_FILE"; then
-        echo "Successfully compressed $DIR" | tee -a "$LOG_FILE"
+    # Compress and encrypt the directory
+    if tar -cf - "${EXCLUDE_OPTIONS[@]}" -C "$(dirname "$DIR")" "$BASENAME" | xz | gpg --batch --yes --passphrase "$ENCRYPTION_KEY" -c -o "$ENCRYPTED_FILE"; then
+        echo "Successfully compressed and encrypted $DIR" | tee -a "$LOG_FILE"
     else
-        echo "Error compressing $DIR" | tee -a "$LOG_FILE"
+        echo "Error compressing and encrypting $DIR" | tee -a "$LOG_FILE"
     fi
 done
 
